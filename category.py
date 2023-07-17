45483a01ddb2f83378b1412af4e7b4f3afdb6c4a
Kiet Tuan <101494254+ktcshust@users.noreply.github.com>
2023-07-17 11:38:18 +0700

    Add files via upload

---
+++ category.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import matplotlib.pyplot as plt
+
+# Khởi tạo ứng dụng Firebase
+cred = credentials.Certificate('bigdata-eec24-firebase-adminsdk-n4a82-94ef80532e.json')  # Thay đổi đường dẫn đến tệp serviceAccountKey.json
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+def get_data():
+    data = {}
+    collection_ref = db.collection('top_20_category')
+    docs = collection_ref.stream()
+    for doc in docs:
+        category = doc.get('category')
+        pledged = float(doc.get('pledged'))
+        data[category] = pledged
+
+    return data
+
+def plot_chart(data):
+    categories = list(data.keys())
+    pledged_values = list(data.values())
+
+
+    fig, ax = plt.subplots()
+    ax.bar(categories, pledged_values)
+    ax.set_xlabel('Category')
+    ax.set_ylabel('Pledged')
+    ax.set_title('Pledged Amount by Category')
+    fig.set_size_inches(10, 6)  # Điều chỉnh kích thước biểu đồ theo ý muốn
+    plt.xticks(rotation=90)
+
+    fig.subplots_adjust(bottom=0.2, top=0.9)
+
+    st.pyplot(fig)
+
+
+# Streamlit interface
+st.title('Pledged Amount by Category')
+
+data = get_data()
+plot_chart(data)
+
