a92490530734e08383d73829e92fd9e8ad982bec
ktcshust <kt209stark@gmail.com>
2023-07-18 00:29:09 +0700

    update

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
+import pandas as pd
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import matplotlib.pyplot as plt
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import scan
 
+
+# Elasticsearch host configuration
+es_hosts = ["http://34.36.145.14:80"]
+
+# Function to perform item search
+def search_items(country, category, es_client=Elasticsearch(hosts=es_hosts[0]), index_name='product_nhom1'):
+    query = {"query": {"bool": {"must": []}}}
+
+    if country:
+        query["query"]["bool"]["must"].append({"match": {"country": country}})
+
+    if category:
+        query["query"]["bool"]["must"].append({"match": {"category": category}})
+
+    scroll = scan(
+        es_client,
+        index=index_name,
+        query=query,
+        scroll='5m',
+        size=10000
+    )
+
+    results = []
+    for hit in scroll:
+        source = hit['_source']
+        results.append(source)
+
+    return results
 # Khởi tạo ứng dụng Firebase
 cred = credentials.Certificate(
     'bigdata-eec24-firebase-adminsdk-n4a82-94ef80532e.json')  # Thay đổi đường dẫn đến tệp serviceAccountKey.json
@@ -99,12 +130,18 @@ def plot_chart_top_20_ratio(data):
 
     st.pyplot(fig)
 
+category_df = pd.read_csv('category.csv')
+category_options = category_df['category'].tolist()
+
+country_df = pd.read_csv('country.csv')
+country_options = country_df['country'].tolist()
+
 
 # Streamlit interface
 st.title('Data Analysis App')
 
 # Create a sidebar navigation menu
-page = st.sidebar.selectbox('Select a page', ['Pledged Amount by Category', 'Top 20 Country', 'Top 20 Ratio'])
+page = st.sidebar.selectbox('Select a page', ['Pledged Amount by Category', 'Top 20 Country', 'Top 20 Ratio', 'Item Search'])
 
 if page == 'Pledged Amount by Category':
     st.header('Pledged Amount by Category')
@@ -121,5 +158,16 @@ elif page == 'Top 20 Ratio':
     data = get_data_top_20_ratio()
     plot_chart_top_20_ratio(data)
 
+elif page == 'Item Search':
+    st.header('Item Search')
 
+    # Input fields for search parameters
+    country = st.selectbox('Country', country_options)
+
+    category = st.selectbox('Category', category_options)
+
+    if st.button('Search'):
+        results = search_items(country, category)
+        for result in results:
+            st.write(result)
 
